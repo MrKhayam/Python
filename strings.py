c659113418f920c674c625c21ac7495ae5cc0f32
unknown <ijazkhayam@gmail.com>
2024-09-24 20:58:29 -0700

    all string functions

--- strings.py
+++ strings.py
@@ -7,6 +7,42 @@
 
 #Ends With Function:
 
-para = "My name is Khayam ijaz";
+# para = "My name is Khayam ijaz";
 
-print(para.endswith("jaz"));
+# print(para.endswith("jaz"));
+
+
+
+
+
+
+#Capitalize function:
+# para = "my name is Khayam";
+# para = para.capitalize();
+# print(para);
+
+
+
+
+#replace function:
+
+# para = "I am a Data Scientist";
+# print(para);
+# para = para.replace("Data Scientist", "Software Engineer");
+# print(para);
+
+
+
+
+#find function (this function returns us the index of 1st letter of the 1st occurence of the given value):
+
+# para = "My name is Khayam ijaz";
+# print(para.find("a"));
+
+
+
+
+#count function (this function tells us theat how many times a word or a letter is repeated in our string):
+
+# para = "My name is Khayam";
+# print(para.count("Khayam"));
