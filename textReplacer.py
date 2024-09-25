0e9e10c759b0bb7bdd157218a689cfdef9889c2c
unknown <ijazkhayam@gmail.com>
2024-09-25 08:10:54 -0700

    tuples

---
+++ textReplacer.py
@@ -0,0 +1,7 @@
+text = input("Enter Your Text:");
+
+toRep = input("Enter the word you want to replace:");
+newWord = input("Enter the new word:");
+
+
+print(text.replace(toRep,newWord));
---
+++ tuple.py
@@ -0,0 +1,4 @@
+names = ("Ryan", "Cillian Murphy", "Ana De Armas", "Robert Downey Junior");
+
+print(names);
+print(type(names));
